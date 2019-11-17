69e081bfff8b272bd935d5f7707a76c7980e2fbb
Maxim <d.zh2012@yandex.ru>
2019-11-17 19:15:35 +0300

    new events, event refactoring

--- Albion.Event/MoveEvent.cs
+++ Albion.Event/MoveEvent.cs
@@ -1,5 +1,4 @@
-﻿using Albion.Common;
-using Albion.Network;
+﻿using Albion.Network;
 using System;
 using System.Collections.Generic;
 
@@ -9,15 +8,79 @@ namespace Albion.Event
     {
         public MoveEvent(Dictionary<byte, object> parameters) : base(parameters)
         {
-            byte[] bytes = (byte[])parameters[1];
-            float x = BitConverter.ToSingle(bytes, 9);
-            float y = BitConverter.ToSingle(bytes, 13);
+            Id = Convert.ToInt64(parameters[0]);
 
-            Id = parameters[0].ToString();
-            Position = new Position(x, y);
+            byte[] array = (byte[])parameters[1];
+            int num = 0;
+            Byte0 = array[num];
+            num++;
+            Ticks = BitConverter.ToInt64(array, num);
+            num += 8;
+            NewPosititon = new float[]
+            {
+                BitConverter.ToSingle(array, num),
+                BitConverter.ToSingle(array, num + 4)
+            };
+            num += 8;
+            Direction = (float)array[num] / 256f * 360f;
+            num++;
+            if ((Byte0 | 0x01) != 0)
+            {
+                Speed = BitConverter.ToSingle(array, num);
+                num += 4;
+            }
+            else
+            {
+                Speed = 0f;
+            }
+            if ((Byte0 | 0x02) != 0)
+            {
+                OldPosititon = new float[]
+                {
+                    BitConverter.ToSingle(array, num),
+                    BitConverter.ToSingle(array, num + 4)
+                };
+                num += 8;
+            }
+            else
+            {
+                OldPosititon = NewPosititon;
+            }
+            if ((Byte0 | 0x04) != 0)
+            {
+                int num2 = array.Length - num;
+                if (num2 == 8)
+                {
+                    Long0 = BitConverter.ToInt64(array, num);
+                }
+                else if (num2 == 4)
+                {
+                    Long0 = BitConverter.ToInt32(array, num);
+                }
+                else if (num2 == 2)
+                {
+                    Long0 = BitConverter.ToInt16(array, num);
+                }
+                else
+                {
+                    throw new Exception("Move decode failure!!!");
+                }
+            }
+            else
+            {
+                Long0 = 0L;
+            }
+            Bool0 = (Byte0 | 0x08) != 0;
         }
 
-        public string Id { get; }
-        public Position Position { get; }
+        public long Id { get; }
+        public byte Byte0 { get; set; }
+        public long Ticks { get; set; }
+        public float[] NewPosititon { get; }
+        public float Direction { get; set; }
+        public float Speed { get; set; }
+        public float[] OldPosititon { get; set; }
+        public long Long0 { get; set; }
+        public bool Bool0 { get; set; }
     }
 }
---
+++ Albion.Operation/JoinOperation.cs
@@ -0,0 +1,20 @@
+﻿using Albion.Network;
+using System;
+using System.Collections.Generic;
+
+namespace Albion.Operation
+{
+    public class JoinOperation : BaseOperation
+    {
+        public JoinOperation(Dictionary<byte, object> parameters) : base(parameters)
+        {
+            UserId = Convert.ToInt64(parameters[0]);
+            Name = Convert.ToString(parameters[2]);
+            LocationId = Convert.ToString(parameters[8]);
+        }
+
+        public long UserId { get; }
+        public string Name { get; }
+        public string LocationId { get; }
+    }
+}
